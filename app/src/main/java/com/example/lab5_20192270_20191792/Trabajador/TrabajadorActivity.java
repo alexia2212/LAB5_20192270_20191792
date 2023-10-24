7cf921ee1e96d8c107e0c13b7ffb61b2019ca4a8
angie2512 <a20191792@pucp.edu.pe>
2023-10-24 15:58:52 -0500

    notificacionesentrabajador

--- app/src/main/java/com/example/lab5_20192270_20191792/Trabajador/TrabajadorActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/Trabajador/TrabajadorActivity.java
@@ -1,14 +1,26 @@
 package com.example.lab5_20192270_20191792.Trabajador;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
+import com.example.lab5_20192270_20191792.MainActivity;
 import com.example.lab5_20192270_20191792.R;
 import com.example.lab5_20192270_20191792.databinding.ActivityTrabajadorBinding;
 
 public class TrabajadorActivity extends AppCompatActivity {
+
+    String channelId = "channelHighPriorTutor";
+
+
 
     ActivityTrabajadorBinding binding;
     @Override
@@ -18,10 +30,28 @@ public class TrabajadorActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
+        lanzarNotificacionInicio();
+
         binding.button7.setOnClickListener(view -> {
             Intent intent = new Intent(TrabajadorActivity.this, DescargarHorarioTrabajador.class);
             startActivity(intent);
 
         });
+    }
+
+    public void lanzarNotificacionInicio(){
+        Intent intent = new Intent(TrabajadorActivity.this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(TrabajadorActivity.this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(TrabajadorActivity.this, channelId)
+                .setSmallIcon(R.drawable.baseline_accessibility_24)
+                .setContentTitle("Empleado")
+                .setContentText("Está entrando en modo Empleado")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(TrabajadorActivity.this);
+        if (ActivityCompat.checkSelfPermission(TrabajadorActivity.this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+            notificationManagerCompat.notify(1, builder.build());
+        }
     }
 }
