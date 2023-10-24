4d254e9a89c982382724160532615e672070dc73
alexia2212 <a20192270@pucp.edu.pe>
2023-10-24 14:38:51 -0500

    aaa

--- app/src/main/java/com/example/lab5_20192270_20191792/MainActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/MainActivity.java
@@ -2,13 +2,24 @@ package com.example.lab5_20192270_20191792;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.lab5_20192270_20191792.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        binding.buttonTrabajador.setOnClickListener(view -> {
+
+            Intent intent= new Intent(MainActivity.this, TrabajadorActivity.class);
+            startActivity(intent);
+        });
     }
 }
