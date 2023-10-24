64b80c4c1c48e37d3966a9d14e558cd307ff97ed
alexia2212 <a20192270@pucp.edu.pe>
2023-10-24 14:19:35 -0500

    Entidades

---
+++ app/src/main/java/com/example/lab5_20192270_20191792/Entity/Departments.java
@@ -0,0 +1,40 @@
+package com.example.lab5_20192270_20191792.Entity;
+
+public class Departments {
+    private Integer departmentId;
+    private String departmentName;
+    private Integer managerId;
+    private Locations locationId;
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Integer getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Integer managerId) {
+        this.managerId = managerId;
+    }
+
+    public Locations getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Locations locationId) {
+        this.locationId = locationId;
+    }
+}
---
+++ app/src/main/java/com/example/lab5_20192270_20191792/Entity/ServiceTrabajador.java
@@ -0,0 +1,4 @@
+package com.example.lab5_20192270_20191792.Entity;
+
+public interface ServiceTrabajador {
+}
