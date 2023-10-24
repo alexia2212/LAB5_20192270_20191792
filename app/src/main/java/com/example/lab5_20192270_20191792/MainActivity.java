303338b22009bee0f2a820ee2aa26c9af27a3ed2
angie2512 <a20191792@pucp.edu.pe>
2023-10-24 15:52:51 -0500

    notificaciontutor

--- app/src/main/java/com/example/lab5_20192270_20191792/MainActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.lab5_20192270_20191792;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +24,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        createNotificationChannelTrabajador();
+        createNotificationChannelTutor();
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
@@ -32,5 +45,38 @@ public class MainActivity extends AppCompatActivity {
         });
 
 
+
+
+    }
+
+
+    String channelId = "channelHighPriorTrabajador";
+    String channelId2 = "channelHighPriorTutor";
+
+    public void createNotificationChannelTrabajador(){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel =new NotificationChannel(channelId, "Canal notificaciones High", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Canal para notificaciones con prioridad Alta");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            askPermission();
+        }
+    }
+
+    public void createNotificationChannelTutor(){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.O){
+            NotificationChannel channel =new NotificationChannel(channelId2, "Canal notificaciones High", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("Canal para notificaciones con prioridad Alta");
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            askPermission();
+        }
+    }
+
+    public void askPermission(){
+        if (Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU &&
+                ActivityCompat.checkSelfPermission(this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_DENIED){
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{POST_NOTIFICATIONS}, 101);
+        }
     }
 }
--- app/src/main/java/com/example/lab5_20192270_20191792/TutorActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/TutorActivity.java
@@ -1,18 +1,30 @@
 package com.example.lab5_20192270_20191792;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static android.Manifest.permission.POST_NOTIFICATIONS;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class TutorActivity extends AppCompatActivity {
 
+    String channelId2 = "channelHighPriorTutor";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutor);
+        lanzarNotificacionInicio();
+
+
 
         Button botondescargar = findViewById(R.id.button7);
         botondescargar.setOnClickListener(new View.OnClickListener() {
@@ -39,6 +51,22 @@ public class TutorActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    public void lanzarNotificacionInicio(){
+        Intent intent = new Intent(TutorActivity.this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(TutorActivity.this, 0, intent, PendingIntent.FLAG_IMMUTABLE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(TutorActivity.this, channelId2)
+                .setSmallIcon(R.drawable.baseline_accessibility_24)
+                .setContentTitle("Tutor")
+                .setContentText("Está entrando en modo Tutor")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(TutorActivity.this);
+        if (ActivityCompat.checkSelfPermission(TutorActivity.this, POST_NOTIFICATIONS) == PackageManager.PERMISSION_GRANTED){
+            notificationManagerCompat.notify(1, builder.build());
+        }
     }
 
 
