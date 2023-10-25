e679b315e3287ad07035b63feab34a4ab8cbc1d6
angie2512 <a20191792@pucp.edu.pe>
2023-10-24 23:50:42 -0500

    tutordescargarlista

--- app/src/main/java/com/example/lab5_20192270_20191792/BuscarTrabajadorActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/BuscarTrabajadorActivity.java
@@ -4,25 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.example.lab5_20192270_20191792.DTOs.BusquedaTrabajadorDto;
-import com.example.lab5_20192270_20191792.Entity.Countries;
-import com.example.lab5_20192270_20191792.Entity.Departments;
 import com.example.lab5_20192270_20191792.Entity.Employees;
-import com.example.lab5_20192270_20191792.Entity.Locations;
-import com.example.lab5_20192270_20191792.Entity.Regions;
 import com.example.lab5_20192270_20191792.databinding.ActivityBuscarTrabajadorBinding;
-import com.example.lab5_20192270_20191792.databinding.ActivityTutorBinding;
-import com.google.gson.Gson;
 
 import java.io.FileOutputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.HashMap;
+import java.io.OutputStreamWriter;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -32,140 +23,66 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class BuscarTrabajadorActivity extends AppCompatActivity {
 
-    String channelId = "channelHighPriorTutor";
     ActivityBuscarTrabajadorBinding binding;
+    BusquedaTrabajadorDto busquedaTrabajadorDto;
+
+    ServiceTrabajador serviceTrabajador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_buscar_trabajador);
-
         binding = ActivityBuscarTrabajadorBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-
-
-        ServiceTrabajador trabajadorService = new Retrofit.Builder()
-                .baseUrl("https://8r7fm6zj-3010.brs.devtunnels.ms")
+        // Configurar Retrofit
+        serviceTrabajador = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3010")
                 .addConverterFactory(GsonConverterFactory.create())
-                .build()
-                .create(ServiceTrabajador.class);
+                .build().create(ServiceTrabajador.class);
 
         binding.descargarDatos.setOnClickListener(view -> {
-                binding.errorID2.setText("");
-                String id = binding.textIDtrabajador.getText().toString();
-                if (!id.equals("")){
-                    try{
-                        int idTrab = Integer.parseInt(id);
-                        trabajadorService.buscarTrabajadorPorId(idTrab).enqueue(new Callback<BusquedaTrabajadorDto>() {
-                            @Override
-                            public void onResponse(Call<BusquedaTrabajadorDto> call, Response<BusquedaTrabajadorDto> response) {
-                                if (response.isSuccessful()){
-                                    BusquedaTrabajadorDto busquedaTrabajadorDto = response.body();
-                                    if (busquedaTrabajadorDto.getRespuesta().equals("no encontrado")){
-                                        binding.errorID2.setText("Empleado inexistente");
-                                    }else {
-                                        HashMap<String, Object> datosDisplay = obtenerDatosDisplay(busquedaTrabajadorDto.getEmpleado());
-                                        guardarDatosComoJson(datosDisplay);
-                                        Toast.makeText(BuscarTrabajadorActivity.this, "Descarga Exitosa!", Toast.LENGTH_SHORT).show();
-                                    }
-                                }else {
-                                    binding.errorID2.setTextSize(24);
-                                    binding.errorID2.setText("No encontrado");
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<BusquedaTrabajadorDto> call, Throwable t) {
-                                binding.errorID2.setText("Trabajador no encontrado");
-                                t.printStackTrace();
-                            }
-                        });
-                    }catch (Exception e){
-                        binding.errorID2.setText("Debe ingresar un número");
-                    }
-                }else {
-                    binding.errorID2.setText("Debe ingresar un ID");
-                }
-            });
-
-        }
-
-
-
-        public HashMap<String, Object> obtenerDatosDisplay(Employees employee){
-
-            HashMap<String, Object> info = new HashMap<>();
-
-            info.put("Id", employee.getEmployeeId().toString());
-
-            String firstName = employee.getFirstName();
-            if (firstName!=null || !firstName.equals(""))
-                info.put("Nombre", employee.getEmployeeId().toString());
-            else
-                info.put("Nombre", "Sin nombre");
-
-            info.put("Apellido",employee.getLastName().toString());
-            info.put("Correo", employee.getEmail().toString());
-
-            String phoneNumber = employee.getPhoneNumber();
-            if (phoneNumber!=null || !phoneNumber.equals(""))
-                info.put("Celular",employee.getPhoneNumber().toString());
-            else
-                info.put("Celular", "Sin celular");
-
-            info.put("Puesto", employee.getJobId().getJobTitle().toString());
-
-            Float salario = employee.getSalary();
-            if (salario!=null)
-                info.put("Salario", String.valueOf(employee.getSalary()));
-            else
-                info.put("Salario", "Sin informacion");
-
-            Departments departmentId = employee.getDepartmentId();
-            if (departmentId==null){
-                info.put("Departamento", "Sin informacion");
-                info.put("Pais", "Sin informacion");
-                info.put("Region", "Sin informacion");
-            }else {
-                info.put("Departamento", employee.getDepartmentId().getDepartmentName().toString());
-                Locations locationId = employee.getDepartmentId().getLocationId();
-                if (locationId==null){
-                    info.put("Pais", "Sin informacion");
-                    info.put("Region", "Sin informacion");
-                }else{
-                    Countries countryId = employee.getDepartmentId().getLocationId().getCountryId();
-                    if (countryId==null || countryId.equals("")){
-                        info.put("Pais", "Sin informacion");
-                        info.put("Region", "Sin informacion");
-                    }else{
-                        info.put("Pais", employee.getDepartmentId().getLocationId().getCountryId().getCountryName().toString());
-                        Regions regionId = employee.getDepartmentId().getLocationId().getCountryId().getRegionsId();
-                        if (regionId==null){
-                            info.put("Region", "Sin informacion");
-                        }else {
-                            info.put("Region", employee.getDepartmentId().getLocationId().getCountryId().getRegionsId().getRegionName().toString());
-                        }
-                    }
-                }
+            String employeeId = binding.textIDtrabajador.getText().toString().trim();
+            if (!employeeId.isEmpty()) {
+                descargarInformacionEmpleado(employeeId);
+            } else {
+                Toast.makeText(this, "Ingrese un ID de empleado", Toast.LENGTH_SHORT).show();
             }
-
-            return info;
-        }
-
-        public void guardarDatosComoJson(HashMap<String, Object> listaDatos){
-            String idTrabajador = (String) listaDatos.get("Id");
-            Gson gson = new Gson();
-            String listaJson = gson.toJson(listaDatos);
-            String fileNameJson = "informacionDe"+idTrabajador+".txt";
-            try(FileOutputStream fileOutputStream = this.openFileOutput(fileNameJson, Context.MODE_PRIVATE);
-                FileWriter fileWriter = new FileWriter(fileOutputStream.getFD());){
-                fileWriter.write(listaJson);
-            }catch (IOException e){
-                e.printStackTrace();
-            }
-        }
-
-
+        });
     }
 
+    private void descargarInformacionEmpleado(String employeeId) {
+        serviceTrabajador.buscarTrabajadorPorId(Integer.parseInt(employeeId)).enqueue(new Callback<BusquedaTrabajadorDto>() {
+            @Override
+            public void onResponse(Call<BusquedaTrabajadorDto> call, Response<BusquedaTrabajadorDto> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    busquedaTrabajadorDto = response.body();
+                    guardarInformacionEnArchivo(employeeId, busquedaTrabajadorDto);
+                } else {
+                    Toast.makeText(BuscarTrabajadorActivity.this, "Empleado no encontrado", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<BusquedaTrabajadorDto> call, Throwable t) {
+                Log.e("msg-test", "Error al descargar la información del empleado", t);
+            }
+        });
+    }
+
+    private void guardarInformacionEnArchivo(String employeeId, BusquedaTrabajadorDto dto) {
+        String fileNameTxt = "informacionDe" + employeeId + ".txt";
+
+        try (FileOutputStream fileOutputStream = openFileOutput(fileNameTxt, Context.MODE_PRIVATE);
+             OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream)) {
+            Employees employee = dto.getEmpleado();
+            writer.write("Información del empleado con ID: " + employeeId + "\n");
+            writer.write("Nombre: " + employee.getFirstName() + " " + employee.getLastName() + "\n");
+            writer.write("Teléfono: " + employee.getPhoneNumber() + "\n");
+            writer.write("Correo electrónico: " + employee.getEmail() + "\n");
+
+            Toast.makeText(BuscarTrabajadorActivity.this, "Información del empleado guardada en " + fileNameTxt, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            Log.e("msg-test", "Error al guardar la información del empleado en el archivo", e);
+        }
+    }
+}
--- app/src/main/java/com/example/lab5_20192270_20191792/ListaTrabajadoresActivity.java
+++ app/src/main/java/com/example/lab5_20192270_20191792/ListaTrabajadoresActivity.java
@@ -2,13 +2,100 @@ package com.example.lab5_20192270_20191792;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.example.lab5_20192270_20191792.DTOs.BusquedaTrabajadoresDto;
+import com.example.lab5_20192270_20191792.Entity.Employees;
+import com.example.lab5_20192270_20191792.databinding.ActivityListaTrabajadoresBinding;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ListaTrabajadoresActivity extends AppCompatActivity {
+
+    ActivityListaTrabajadoresBinding binding;
+    List<Employees> listadeEmpleados = new ArrayList<>();
+    String codigoManager;
+
+    ServiceTrabajador serviceTrabajador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_trabajadores);
+
+        binding = ActivityListaTrabajadoresBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        servicio();
+
+        binding.descargarDatos.setOnClickListener(view -> {
+            codigoManager = String.valueOf(binding.textIDtutor.getText());
+            if(codigoManager.length()>0){
+                lista(codigoManager);
+            }else{
+                Toast.makeText(this, "Ingresar codigo", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    public void servicio(){
+        serviceTrabajador = new Retrofit.Builder()
+                .baseUrl("http://10.0.2.2:3010")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build().create(ServiceTrabajador.class);
+    }
+
+    public void lista(String codigoManager) {
+
+        serviceTrabajador.buscarTrabajadorTodos(Integer.parseInt(codigoManager)).enqueue(new Callback<List<Employees>>() {
+            @Override
+            public void onResponse(Call<List<Employees>> call, Response<List<Employees>> response) {
+                listadeEmpleados = response.body();
+
+                guardarLista(listadeEmpleados);
+                Toast.makeText(ListaTrabajadoresActivity.this, "Descarga de trabajadores exitosa", Toast.LENGTH_SHORT).show();
+                Log.d("msg-test", "Se guardo la lista");
+
+            }
+
+            @Override
+            public void onFailure(Call<List<Employees>> call, Throwable t) {
+                Log.d("msg-test", "Algo paso");
+                Log.d("msg-test", t.getMessage());
+
+            }
+        });
+
+    }
+
+    public void guardarLista(List<Employees> lista) {
+        String fileNameTxt = "listaTrabajadores.txt";
+
+        try (FileOutputStream fileOutputStream = openFileOutput(fileNameTxt, Context.MODE_PRIVATE);
+             OutputStreamWriter writer = new OutputStreamWriter(fileOutputStream)) {
+            writer.write("Trabajadores bajo el mando del tutorid: "+codigoManager+"\n");
+            for (Employees employee : lista) {
+
+                writer.write(employee.getFirstName()+" "+ employee.getLastName() +" "+ employee.getPhoneNumber() +" "+ employee.getEmail() + "\n");
+            }
+
+            Log.d("msg-test", "Archivo de texto guardado correctamente");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
